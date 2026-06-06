0296724419c2758b8a4709f3c9a46131789f7d41
ELVERRUEDA <elver.rueda@cun.edu.co>
2026-06-06 15:41:04 -0500

    security: mover credenciales a archivo separado excluido de git

---
+++ src/credentials.example.h
@@ -0,0 +1,12 @@
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+// WiFi
+const char* ssid = "TU_SSID";
+const char* password = "TU_PASSWORD";
+
+// Firebase
+const char* firebase_project_id = "TU_PROJECT_ID";
+const char* authToken = "";
+
+#endif
